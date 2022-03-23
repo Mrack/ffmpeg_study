f394232469470ce91990f2c1a5301d79ca90afd0
Mrack <Mrack@qq.com>
2022-03-24 00:14:44 +0800

    录屏数据

--- camera1/app/src/main/java/com/example/camera1/SocketPush.java
+++ camera1/app/src/main/java/com/example/camera1/SocketPush.java
@@ -8,8 +8,10 @@ import android.media.projection.MediaProjection;
 import android.view.Surface;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 
-public class SocketPush {
+public class SocketPush extends Thread {
     private MediaProjection mediaProjection;
     private MediaCodec codec;
     private int width = 720;
@@ -26,17 +28,38 @@ public class SocketPush {
         this.mediaProjection = mediaProjection;
         try {
             MediaFormat videoFormat = MediaFormat.createVideoFormat(MediaFormat.MIMETYPE_VIDEO_HEVC, width, height);
-            videoFormat.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
-            videoFormat.setInteger(MediaFormat.KEY_BIT_RATE, width * height);
+            videoFormat.setInteger(MediaFormat.KEY_COLOR_FORMAT,
+                    MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
+            videoFormat.setInteger(MediaFormat.KEY_BIT_RATE, width*height);
             videoFormat.setInteger(MediaFormat.KEY_FRAME_RATE, 20);
             videoFormat.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 1);
-            codec = MediaCodec.createEncoderByType("video/hevc");
+            codec = MediaCodec.createEncoderByType(MediaFormat.MIMETYPE_VIDEO_HEVC);
             codec.configure(videoFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
             Surface inputSurface = codec.createInputSurface();
-            mediaProjection.createVirtualDisplay("ttt", width, height, 1, DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC, inputSurface, null, null);
-            codec.start();
+            mediaProjection.createVirtualDisplay("-display", width, height, 1, DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC, inputSurface, null, null);
+
+
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        start();
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        codec.start();
+        while (true) {
+            MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
+            int index = codec.dequeueOutputBuffer(bufferInfo, 10000);
+            while (index >= 0) {
+                ByteBuffer outputBuffer = codec.getOutputBuffers()[index];
+                byte[] bytes = new byte[bufferInfo.size];
+                outputBuffer.get(bytes);
+                System.out.println(Arrays.toString(bytes));
+                codec.releaseOutputBuffer(index, false);
+                index = codec.dequeueOutputBuffer(bufferInfo, 10000);
+            }
         }
     }
 }
